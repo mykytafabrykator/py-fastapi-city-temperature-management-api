16c928122e83e427bc6918f85a1ab2d39f8bd9f6
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-10 14:14:04 +0200

    Add database configuration

---
+++ core/database.py
@@ -0,0 +1,31 @@
+import os
+from typing import AsyncGenerator
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker
+)
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    connect_args={
+        "check_same_thread": False
+    } if "sqlite" in DATABASE_URL else {},
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
