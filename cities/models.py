45be5ebf5d3a94331a087a7d338df752faab259f
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-10 14:46:25 +0200

    Add City model

---
+++ cities/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    additional_info: Mapped[str | None] = mapped_column(
+        String(500),
+        nullable=True
+    )
