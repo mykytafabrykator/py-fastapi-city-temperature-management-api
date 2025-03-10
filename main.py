f77f4d12d2076acb4e2bae769fb0e3d8c27ffb89
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-10 16:12:17 +0200

    Update routes in main with temperature router

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, APIRouter
 from cities.routes import router as cities_router
+from temperature.routes import router as temperature_router
 
 app = FastAPI()
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(cities_router)
+app.include_router(temperature_router)
 
 app.include_router(api_router)
 
